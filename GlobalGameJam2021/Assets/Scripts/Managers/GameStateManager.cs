7f7a271864dc720274c1b7488d5a8b846839aa4f
Robin Bono <robin@omnick.se>
2021-01-28 16:29:53 +0100

    Update GameStateManager.cs

--- GlobalGameJam2021/Assets/Scripts/Managers/GameStateManager.cs
+++ GlobalGameJam2021/Assets/Scripts/Managers/GameStateManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[DefaultExecutionOrder(-9)]
 public class GameStateManager : MonoBehaviour
 {
     public static GameStateManager instance;
