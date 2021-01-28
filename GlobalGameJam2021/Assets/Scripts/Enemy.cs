54ea57c713f060518c3939495c42a7eae2a4dc3c
Robin Bono <robin@omnick.se>
2021-01-28 15:33:22 +0100

    Update Enemy.cs
    added event to game manager

--- GlobalGameJam2021/Assets/Scripts/Enemy.cs
+++ GlobalGameJam2021/Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,8 @@ public class Enemy : Character
         if (other.tag == "Relic")
         {
             Pickup(other.GetComponent<Relic>());
+            
+            GameManager.instance.PickedUpObject(this.gameObject, other.gameObject);
         }
     }
 }
