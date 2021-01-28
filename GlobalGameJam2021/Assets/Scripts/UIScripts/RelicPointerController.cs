773a54dbaa3ac6852c053792341aa04c388b18ed
Wiive <wirrechan@hotmail.com>
2021-01-28 16:15:16 +0100

    Updated RelicUI

--- GlobalGameJam2021/Assets/Scripts/UIScripts/RelicPointerController.cs
+++ GlobalGameJam2021/Assets/Scripts/UIScripts/RelicPointerController.cs
@@ -7,31 +7,40 @@ public class RelicPointerController : MonoBehaviour
 {
     public Image targetSymbol;
     public Image arrow;
+    public bool isActive;
 
     public Transform target;
 
-    public float speed = 1.0f;
     [SerializeField] int angleOffset;
 
     private void Update()
     {
-        Vector3 targetDirection = target.transform.position - Camera.main.WorldToScreenPoint(arrow.rectTransform.position);
+        if (isActive == true)
+        {
+            gameObject.SetActive(true);
+            RotateArrow();
+        }
+        else
+            gameObject.SetActive(false);
+    }
+
+    private void RotateArrow()
+    {
+        Vector3 targetDirection = target.position - arrow.rectTransform.position;
 
         var angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg + angleOffset;
 
-        Debug.DrawRay(targetDirection * 10000, arrow.rectTransform.position, Color.red);
-
-        //float singleStep = speed * Time.deltaTime;
-
-        //Vector3 newDirection = Vector3.RotateTowards(arrow.transform.up, targetDirection, singleStep, 0.0f);
-
-        //Debug.DrawRay(arrow.transform.position, newDirection, Color.red);
-
-        arrow.rectTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        //arrow.transform.rotation = Quaternion.LookRotation(newDirection);
+        arrow.rectTransform.rotation = Quaternion.AngleAxis(angle + angleOffset * 2, Vector3.forward);
     }
 
+    public void EnableRelicArrow(RelicPointerController relicArrowObject)
+    {
+        relicArrowObject.isActive = true;
+    }
 
+    public void DisableRelicArrow(RelicPointerController relicArrowObject)
+    {
+        relicArrowObject.isActive = false;
+    }
 
 }
