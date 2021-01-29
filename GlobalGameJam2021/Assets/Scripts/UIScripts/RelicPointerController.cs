c080c33533f18e2f5dc66f31585e656ee32cfd33
Wiive <wirrechan@hotmail.com>
2021-01-29 11:48:41 +0100

    Updated RelicPointer

--- GlobalGameJam2021/Assets/Scripts/UIScripts/RelicPointerController.cs
+++ GlobalGameJam2021/Assets/Scripts/UIScripts/RelicPointerController.cs
@@ -7,48 +7,80 @@ public class RelicPointerController : MonoBehaviour
 {
     public Image targetSymbol;
     public Image arrow;
+
+    public int positionOffset = 16;
+
+    private int arrowOffset = 25;
+
     public bool isActive;
 
-    public Transform target;
+    public Relic target;
 
     [SerializeField] int angleOffset;
 
     private void Update()
     {
-
-
-        if (isActive == true)
+        if(isActive == true && !IsRelicVisibleOnScreen())
         {
-            targetSymbol.enabled = true;
-            arrow.enabled = true;
+            SetRelicPointerVisable(true);
 
             RotateArrow();
-        }
-        else
-        {
-            targetSymbol.enabled = false;
-            arrow.enabled = false;
+            MoveRelicPointerObject();
         }
 
+        else
+        {
+            SetRelicPointerVisable(false);
+        }
+    }
+
+    bool IsRelicVisibleOnScreen()
+    {
+        Vector2 cameraPosition = Camera.main.transform.position;
+        Vector2 relicPosition = target.transform.position;
+
+        float width = 320 / 2;
+        float height = 180 / 2;
+        float relicSize = 16 / 2;
+
+        if (relicPosition.x < cameraPosition.x - (width + relicSize) ||
+            relicPosition.x > cameraPosition.x + (width + relicSize) ||
+            relicPosition.y < cameraPosition.y - (height + relicSize) ||
+            relicPosition.y > cameraPosition.y + (height + relicSize))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void SetRelicPointerVisable(bool show)
+    {
+        targetSymbol.enabled = show;
+        arrow.enabled = show;
     }
 
     private void RotateArrow()
     {
-        Vector3 targetDirection = target.position - arrow.rectTransform.position;
+        Vector3 targetDirection = target.transform.position - arrow.rectTransform.position;
 
         var angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg + angleOffset;
 
         arrow.rectTransform.rotation = Quaternion.AngleAxis(angle + angleOffset * 2, Vector3.forward);
     }
 
-    public void EnableRelicArrow(RelicPointerController relicArrowObject)
+    private void MoveRelicPointerObject()
     {
-        relicArrowObject.isActive = true;
-    }
+        Vector2 cameraPosition = Camera.main.transform.position;
+        Vector2 relicPosition = target.transform.position;
 
-    public void DisableRelicArrow(RelicPointerController relicArrowObject)
-    {
-        relicArrowObject.isActive = false;
-    }
+        float width = 320 / 2;
+        float height = 180 / 2;
+        float arrowSize = 16 / 2;
 
+        float y = Mathf.Clamp(relicPosition.y, cameraPosition.y - (height - arrowSize), cameraPosition.y + (height - arrowSize));
+        float x = Mathf.Clamp(relicPosition.x, cameraPosition.x - (width - arrowSize), cameraPosition.x + (width - arrowSize));
+
+        transform.position = new Vector3(x, y, 0);
+    }
 }
