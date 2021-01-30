ee27638df049656fe12d7091e8dd49b981573b82
Wiive <wirrechan@hotmail.com>
2021-01-30 12:43:26 +0100

    Updated MenuManager

--- GlobalGameJam2021/Assets/Scripts/UIScripts/MenuManager.cs
+++ GlobalGameJam2021/Assets/Scripts/UIScripts/MenuManager.cs
@@ -6,16 +6,59 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject audioMenu;
+    public GameObject gameOverMenu;
+    public GameObject pauseMenu;
 
-   public void ShowMainMenu()
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            GameStateManager.instance.ChangeGameState(GameStateManager.GameState.IngameMenu);
+            ShowPauseMenus();
+        }
+        
+
+    }
+
+
+    public void ShowMainMenu()
     {
         mainMenu.SetActive(true);
         audioMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        pauseMenu.SetActive(false);
     }
 
     public void ShowAdioMenu()
     {
         mainMenu.SetActive(false);
         audioMenu.SetActive(true);
+        gameOverMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+
+    public void ShowGameOverMenu()
+    {
+        mainMenu.SetActive(false);
+        audioMenu.SetActive(false);
+        gameOverMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void ShowPauseMenus()
+    {
+        mainMenu.SetActive(false);
+        audioMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void CloseAllMenus()
+    {
+        mainMenu.SetActive(false);
+        audioMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        pauseMenu.SetActive(false);
     }
 }
